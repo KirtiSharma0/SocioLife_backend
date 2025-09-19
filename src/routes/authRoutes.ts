7e98acac4bf851945253bac9e8d188590d5616cd
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-09-19 23:51:41 +0530

    initial commit

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,14 +1,10 @@
-import { Router } from "express";
-import {
-  createUser,
-  loginUser,
-  logoutUser,
-} from "../controllers/auth.controller";
+ import { Router } from "express";
+import { signUp, signIn, logout } from "../controllers/auth.controller";
 
-const authRouter = Router();
+const authRouter: Router = Router();
 
-authRouter.post("/signup", createUser);
-authRouter.post("/signin", loginUser);
-authRouter.post("/logout", logoutUser);
+authRouter.post('/signup', signUp);
+authRouter.post('/signin', signIn);
+authRouter.post('/logout', logout);
 
 export default authRouter;
